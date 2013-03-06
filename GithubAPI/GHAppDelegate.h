ac0db68d86241035b4691913fd3fe913e808a781
Oliver Letterer <oliver.letterer@gmail.com>
2013-03-06 20:10:28 +0100

    Initial Commit

---
+++ GithubAPI/GHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GHAppDelegate.h
+//  GithubAPI
+//
+//  Created by Oliver Letterer on 06.03.13.
+//  Copyright (c) 2013 Sparrow-Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GithubAPI/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GithubAPI
+//
+//  Created by Oliver Letterer on 06.03.13.
+//  Copyright (c) 2013 Sparrow-Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GHAppDelegate class]));
+    }
+}
